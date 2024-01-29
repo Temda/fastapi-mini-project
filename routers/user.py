cfcf39043dc780f30de254dc180ca086405a41d5
NkTheRipper <tenda2526@gmail.com>
2024-01-30 02:53:47 +0700

    CRUD with database Sqlite

--- routers/user.py
+++ routers/user.py
@@ -1,8 +1,10 @@
-# routers/user.py
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from config.database import SessionLocal, engine, Base
-from schemas.user import UserCreate
+from schemas.user import UserCreate, UserUpdate
+
 from model.user import User
 import bcrypt
 
@@ -41,3 +43,27 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db)):
 @router.get("/users/")  
 async def read_users(db: Session = Depends(get_db)):
     return db.query(User).all()
+
+
+@router.patch("/users/{user_id}", response_model=UserUpdate)
+async def update_user(user_id: str, user: UserUpdate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    db_user.username = user.username
+    db_user.email = user.email
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+@router.delete('/users/{user_id}')
+async def delete_user(user_id: str, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    db.delete(db_user)
+    db.commit()
+    return {"message": "User successfully deleted", "user_id": user_id}
